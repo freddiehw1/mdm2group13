c7450875ddbcea8f4a8e74ed3f4c874cf5ea9976
freddiehw1 <98822998+freddiehw1@users.noreply.github.com>
2022-12-01 13:42:31 +0000

    Add files via upload

--- church_monthly.py
+++ church_monthly.py
@@ -5,25 +5,25 @@ Created on Sat Nov 26 19:00:04 2022
 
 @author: freddie
 """
-
-external_temp = [5,5,7,9,12,15,17,17,15,11,8,6]
-ground_temp = 10
+# Adjustable Variables
 
 
-flow_velocity = 4
-internal_temp = 20
-door_open = [3, 4, 5]
-CSA = 1.6
-time = list(range(0,12))
-heat_on = 0
-heat_off = 100
-time_open = 0.083
-Cp = 1000
-density = 1.204
 
-A_walls = 769.28
-A_roof = 640
-A_ground = 320
+
+hours_heating_on = 720
+hours_door_opens = 140
+time_open = 0.083       #This is a fraction of an hour representing time door open per hour
+d = 30  # Days a month that the heating turns on
+
+
+flow_velocity = 4       #This is wind speed in m/s
+
+internal_temp = 20      # this is the desired internal temperature
+
+external_temp = [5.26,5,7,9,12,15,17,17,15,11,8,6]     #This is the list of average temperatures - 1 for each month
+ground_temp = 10.4
+CSA = 1.6           # Area of the door
+
 
 
 U_walls = 2
@@ -31,18 +31,35 @@ U_roof =  2.5
 U_ground = 1.3
 
 
+# Non Adjustable Variables
+
+A_walls = 769.28
+A_roof = 640
+A_ground = 320
+
+
+Cp = 1000
+density = 1.204
+Volume = 3694
+M = density * Volume
+
+
+
+time = list(range(0,12))
+
+
+
+
 
 gas = 0.103
 elec = 0.34
-
 
 totalg = 0
 totale = 0 
 Ce_list = []
 Cg_list = []
 
-
-
+# Check UNits of Cp 
 
 for x in time:
     
@@ -54,12 +71,14 @@ for x in time:
         Ht_ground = (A_ground * U_ground * (internal_temp - ground_temp))/1000
         
         Ht = Ht_walls + Ht_roof + Ht_ground
-        Ht = Ht * 700
+        
         
         vol_flow = flow_velocity * CSA
-        Hv = (time_open * Cp * density * vol_flow*(internal_temp - external_temp[x]))/1000
-        Hv = Hv * 30 
-        H_total = Hv +Ht
+        Hv = (Cp * density * vol_flow*(internal_temp - external_temp[x]))/1000
+        
+        q = M * Cp * (internal_temp - external_temp[x])/(3600*1000)
+
+        H_total = (Ht * hours_heating_on)+(time_open * Hv * hours_door_opens) + (d*q)
       
         
         Cost_gas = H_total * gas
@@ -70,7 +89,7 @@ for x in time:
         Cg_list.append(Cost_gas)
         totalg += Cost_gas
         totale += Cost_elec
-        print(Cost_gas)
+        
         
         
     
